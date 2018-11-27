c8013271bad59d81c4177396e4d105191d978df7
Andrey Klimov <anklimsk@gmail.com>
2018-11-27 15:39:13 +0300

    new: dev: Added configuration files

---
+++ app/Config/config.php
@@ -0,0 +1,65 @@
+<?php
+$config = array (
+  'Config' => 
+  array (
+    'language' => 'eng',
+    'adminEmail' => 'admin@localhost',
+  ),
+  'WPKG' => 
+  array (
+    'ExternalAuth' => false,
+    'AdminGroupMember' => '',
+    'Company' => '',
+    'SearchBase' => '',
+    'AutocompleteLimit' => 30,
+    'EmailNotifyUser' => false,
+    'EmailContact' => 'admin@localhost',
+    'EmailSubject' => 'wpkgExpress II',
+    'EmailSmtphost' => 'localhost',
+    'EmailSmtpport' => 25,
+    'EmailSmtpuser' => '',
+    'EmailSmtppassword' => '',
+    'IntAuthUser' => 'admin',
+    'IntAuthPassword' => '',
+    'FormatXml' => true,
+    'ExportDisable' => false,
+    'ExportNotes' => true,
+    'ExportDisabled' => true,
+    'ProtectXml' => true,
+    'XmlAuthUser' => 'XmlUser',
+    'XmlAuthPassword' => '',
+    'AutoVarRevision' => true,
+    'SmbAuthUser' => '',
+    'SmbAuthPassword' => '',
+    'SmbServer' => '',
+    'SmbWorkgroup' => '',
+    'SmbLogShare' => '',
+    'SmbDbShare' => '',
+    'SearchBaseUser' => '',
+    'SearchBaseComp' => '',
+    'DefaultSearchAnyPart' => true,
+  ),
+  'CakeSearchInfo' => 
+  array (
+    'AutocompleteLimit' => 30,
+    'DefaultSearchAnyPart' => true,
+  ),
+  'CakeTheme' => 
+  array (
+    'ViewExtension' => 
+    array (
+      'AutocompleteLimit' => 30,
+    ),
+  ),
+  'CakeLdap' => 
+  array (
+    'LdapSync' => 
+    array (
+      'SearchBase' => '',
+    ),
+  ),
+  'Email' => 
+  array (
+    'live' => false,
+  ),
+);
