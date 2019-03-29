7872d82da40a52c421fb07044d8d2d3a856a9672
Andrey Klimov <anklimsk@gmail.com>
2019-03-29 09:57:34 +0300

    chg: dev: Updated constant `PROJECT_VERSION`

--- app/Config/project_constants.php
+++ app/Config/project_constants.php
@@ -61,7 +61,7 @@ if (!defined('PROJECT_AUTHOR')) {
  *  Default value ``
  */
 if (!defined('PROJECT_VERSION')) {
-	define('PROJECT_VERSION', '1.0.7');
+	define('PROJECT_VERSION', '1.0.8');
 }
 
 /**
