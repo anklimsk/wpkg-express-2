ed12feb619cf00b3e44547ea15675e926b42b43c
Andrey Klimov <anklimsk@gmail.com>
2019-12-08 16:47:29 +0300

    chg: dev: Updated `composer/installers`

--- app/Vendor/composer/autoload_real.php
+++ app/Vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit7fbc2bc05faa5ea6489f187cc626963e
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit7fbc2bc05faa5ea6489f187cc626963e', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
